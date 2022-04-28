22436293c1faf929b1a6581152108c3140d3f0a2
berkan-alci <78726455+berkan-alci@users.noreply.github.com>
2022-04-28 17:33:40 +0200

    fixed register bug

--- base/models.py
+++ base/models.py
@@ -2,49 +2,54 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 
+
 class User(AbstractUser):
-    id = models.UUIDField(primary_key= True, default = uuid.uuid4, editable= False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200, null=True)
-    username = models.CharField(unique=True, max_length=200, null = True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True, default="avatar.svg")
 
-   
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
 class Topic(models.Model):
-    id = models.UUIDField(primary_key= True, default = uuid.uuid4, editable= False)
-    name = models.CharField(max_length = 200)
-    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+
     def __str__(self):
         return self.name
 
+
 class Room(models.Model):
-    id = models.UUIDField(primary_key= True, default = uuid.uuid4, editable= False)
-    host = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
-    participants = models.ManyToManyField(User, related_name = 'participants', blank = True) 
-    topic =  models.ForeignKey(Topic, on_delete = models.SET_NULL, null = True)
-    name = models.CharField(max_length = 200)
-    description = models.TextField(null = True, blank = True)
-    updated = models.DateTimeField(auto_now = True)
-    created_at = models.DateTimeField(auto_now_add = True )
-    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ['-updated', '-created_at']
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class Message(models.Model):
-    id = models.UUIDField(primary_key= True, default = uuid.uuid4, editable= False)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    room = models.ForeignKey(Room, on_delete = models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    updated = models.DateTimeField(auto_now = True)
-    created_at = models.DateTimeField(auto_now_add = True )
-    
+    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ['-updated', '-created_at']
-    
+
     def __str__(self):
         return self.body[0:50]
